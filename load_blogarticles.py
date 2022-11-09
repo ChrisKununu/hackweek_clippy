59dc6ae9c540d61ff0cd9f831bc1c8e5e8b43bb0
christoph.haushofer <christoph.haushofer@kununu.com>
2022-11-09 16:01:09 +0100

    added method for loading blog articles

--- load_blogarticles.py
+++ load_blogarticles.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import xml.etree.ElementTree as ET
+import yaml
 
 
 def parse_xml(path):
@@ -22,18 +23,22 @@ def load_data(r):
                              "title": title,
                              "content": results,
                              "pubDate": x.find('pubDate').text,
-                             "link": x.find('link').text,
+                             "link": x[4].text,
                              "author": x[3].text})
     return data
 
 
 if __name__ == '__main__':
-    file_path = 'data/blog_data/kununublog.WordPress.2022-11-08.xml'
+
+    with open('config/config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    file_path = config['input_file_path']
     root = parse_xml(file_path)
     blog_data = load_data(root)
     df = pd.DataFrame(blog_data)
     print(f"Loaded {df.shape[0]} Articles!")
 
-    with open('data/blog_data/kununublog.parquet', mode='wb') as file:
+    with open(config['output_file_path'], mode='wb') as file:
         df.to_parquet(file)
 
