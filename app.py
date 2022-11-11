7b89f43d2fd81ffc6a82a92434624e6b475a9ffc
Paul Leitner <paul.leitner89@gmail.com>
2022-11-11 13:32:05 +0100

    :sparkle: :zap: :boom: QA with blog also works, new load function for the reviews index

--- app.py
+++ app.py
@@ -47,16 +47,18 @@ def setup() -> tuple:
     return credentials, config
 
 
-@st.cache(ttl=600, allow_output_mutation=True)
+@st.cache(ttl=900, allow_output_mutation=True)
 def data_setup(
     ds_credentials,
-    ds_config):
+    ds_config,
+    index_name: str = 'index_name'
+):
 
     # init a DocumentStore
     document_store = OpenSearchDocumentStore(host=ds_credentials['host'],
                                              username=ds_credentials['user'],
                                              password=ds_credentials['password'],
-                                             index=ds_config['index_name'])
+                                             index=ds_config[index_name])
 
     # Loads reader and retriever
     retriever = BM25Retriever(document_store=document_store)
@@ -72,7 +74,8 @@ def data_setup(
 def main():
 
     credentials, config = setup()
-    pipe = data_setup(credentials, config)
+    pipe_blog = data_setup(credentials, config, index_name='index_name')
+    pipe_reviews = data_setup(credentials, config, index_name='review_index')
 
     # Persistent state
     set_state_if_absent("question", DEFAULT_QUESTION_AT_STARTUP)
@@ -102,75 +105,136 @@ def main():
         "Min. Score for valid results",
         min_value=0,
         max_value=100,
-        value=50,
+        value=20,
         step=1,
         on_change=reset_results,
         )
 
-    with st.spinner(
-            "🧠 &nbsp;&nbsp; Setting up the system... \n "
-            ):
+    blog, review = st.tabs(["blog insights", "FERCHAU review QA"])
 
 
-        # Title
-        st.write("# Clippy Demo - Explore the blog")
-        st.markdown(
-            """
-    This demo takes its data from news.kununu.com pages crawled in November 2022.
-
-    Ask any question on this topic and see if Clippy can find the correct answer to your query!
-
-    *Note: do not use keywords, but full-fledged questions.* The demo is not optimized to deal with keyword queries and might misunderstand you.
-    """,
-            unsafe_allow_html=True,
-            )
-
-        search_term = st.text_input(
-            value=st.session_state.question,
-            max_chars=100,
-            on_change=reset_results,
-            label='Enter your Question:',
-            label_visibility="hidden"
-            )
-
-
-
-    if search_term:
+    with blog:
         with st.spinner(
-                "🧠 &nbsp;&nbsp; Performing neural search on documents... \n "
+                "🧠 &nbsp;&nbsp; Setting up the system... \n "
                 ):
+            # Title
+            st.write("## Explore the blog")
+            st.markdown(
+                """
+        This demo takes its data from news.kununu.com pages crawled in November 2022.
 
-            prediction = pipe.run(
-                query=search_term.lower(),
-                params={"Retriever": {"top_k": top_k_retriever}, "Reader": {"top_k": top_k_reader}}
-                )
+        Ask any question on this topic and see if Clippy can find the correct answer to your query!
 
-            for i in range(len(prediction['answers'])):
-                answer = prediction['answers'][i].answer
-                context = prediction['answers'][i].context
-                start_idx = context.find(answer)
-                end_idx = start_idx + len(answer)
-                score = round(prediction['answers'][i].score * 100, 2)
-                document_id = prediction['answers'][i].document_id
-                doc = [x for x in prediction['documents'] if x.id == document_id][0]
-                meta = doc.meta
-                url, title = meta['link'], meta['title']
-                source = f"[{title}]({url})"
+        *Note: do not use keywords, but full-fledged questions.* The demo is not optimized to deal with keyword queries and might misunderstand you.
+        """,
+                unsafe_allow_html=True,)
 
-                if score >= float(min_score):
-                    # Hack due to this bug: https://github.com/streamlit/streamlit/issues/3190
-                    st.write(
-                        markdown(context[:start_idx] + str(annotation(answer, "ANSWER", "#8ef")) + context[end_idx:]),
-                        unsafe_allow_html=True,
-                        )
-                    st.markdown(f"**Relevance:** {score} -  **Source:** {source}")
-                #else:
-                #    st.info(
-                #        "🤔 &nbsp;&nbsp; Clippy is unsure whether any of the documents contain an answer to your question. Try to reformulate it!"
-                #        )
-                #    st.write("**Relevance:** ", score)
+            search_term = st.text_input(
+                value=st.session_state.question,
+                max_chars=100,
+                on_change=reset_results,
+                label='Enter your Question:',
+                label_visibility="hidden",
+                key='search_term_blog')
+
+        if search_term:
+            with st.spinner(
+                    "🧠 &nbsp;&nbsp; Performing neural search on documents... \n "
+                    ):
+
+                blog_prediction = pipe_blog.run(
+                    query=search_term.lower(),
+                    params={"Retriever": {"top_k": top_k_retriever}, "Reader": {"top_k": top_k_reader}}
+                    )
+
+                for i in range(len(blog_prediction['answers'])):
+                    answer = blog_prediction['answers'][i].answer
+                    context = blog_prediction['answers'][i].context
+                    start_idx = context.find(answer)
+                    end_idx = start_idx + len(answer)
+                    score = round(blog_prediction['answers'][i].score * 100, 2)
+                    document_id = blog_prediction['answers'][i].document_id
+                    doc = [x for x in blog_prediction['documents'] if x.id == document_id][0]
+                    meta = doc.meta
+                    url, title = meta['link'], meta['title']
+                    source = f"[{title}]({url})"
+
+                    if score >= float(min_score):
+                        # Hack due to this bug: https://github.com/streamlit/streamlit/issues/3190
+                        st.write(
+                            markdown(context[:start_idx] + str(annotation(answer, "ANSWER", "#8ef")) + context[end_idx:]),
+                            unsafe_allow_html=True,
+                            )
+                        st.markdown(f"**Relevance:** {score} -  **Source:** {source}")
+                    #else:
+                    #    st.info(
+                    #        "🤔 &nbsp;&nbsp; Clippy is unsure whether any of the documents contain an answer to your question. Try to reformulate it!"
+                    #        )
+                    #    st.write("**Relevance:** ", score)
+
+    with review:
+        with st.spinner(
+                "🧠 &nbsp;&nbsp; Setting up the system... \n "
+                ):
+            # Title
+            st.write("## find out about Ferchau as an Employer")
+            st.write('[Profile Link](https://www.kununu.com/de/ferchau)')
+            st.markdown(
+                """
+        This demo takes its data from Employer reviews about Ferchau.
+
+        Ask any question on this topic and see if Clippy can find the correct answer to your query!
+
+        *Note: do not use keywords, but full-fledged questions.* The demo is not optimized to deal with keyword queries and might misunderstand you.
+        """,
+                unsafe_allow_html=True,)
+
+            search_term = st.text_input(
+                value=st.session_state.question,
+                max_chars=100,
+                on_change=reset_results,
+                label='Enter your Question:',
+                label_visibility="hidden",
+                key='search_term_review')
+
+        if search_term:
+            with st.spinner(
+                    "🧠 &nbsp;&nbsp; Performing neural search on documents... \n "
+                    ):
+
+                prediction = pipe_reviews.run(
+                    query=search_term.lower(),
+                    params={"Retriever": {"top_k": top_k_retriever}, "Reader": {"top_k": top_k_reader}}
+                    )
+
+                for i in range(len(prediction['answers'])):
+                    answer = prediction['answers'][i].answer
+                    context = prediction['answers'][i].context
+                    start_idx = context.find(answer)
+                    end_idx = start_idx + len(answer)
+                    score = round(prediction['answers'][i].score * 100, 2)
+                    document_id = prediction['answers'][i].document_id
+                    doc = [x for x in prediction['documents'] if x.id == document_id][0]
+                    meta = doc.meta
+                    url, title = meta['review_url'], meta['category']
+                    source = f"[{title}]({url})"
+
+                    if score >= float(min_score):
+                        # Hack due to this bug: https://github.com/streamlit/streamlit/issues/3190
+                        st.write(
+                            markdown(context[:start_idx] + str(annotation(answer, "ANSWER", "#8ef")) + context[end_idx:]),
+                            unsafe_allow_html=True,
+                            )
+                        st.markdown(f"**Relevance:** {score} -  **Source:** {url}")
+                    #else:
+                    #    st.info(
+                    #        "🤔 &nbsp;&nbsp; Clippy is unsure whether any of the documents contain an answer to your question. Try to reformulate it!"
+                    #        )
+                    #    st.write("**Relevance:** ", score)
 
 
 if __name__ == '__main__':
+
+    st.write('# Clippy Demo')
     main()
 
--- reviews_index.py
+++ reviews_index.py
@@ -2,7 +2,11 @@ from haystack.document_stores import OpenSearchDocumentStore
 from haystack.schema import Document
 import pandas as pd
 import yaml
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 with open('./config/config.yaml', mode='r') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
@@ -10,21 +14,46 @@ with open('./config/config.yaml', mode='r') as file:
 with open('./config/credentials.yaml', mode='r') as file:
     credentials = yaml.load(file, Loader=yaml.FullLoader)
 
-ds = OpenSearchDocumentStore(
-    host=credentials['host'],
-    username=credentials['user'],
-    password=credentials['password'],
-    index=config['review_index'])
 
-relevant_cols = (
-    'text_s1',
+relevant_cols = [
     'text_s1_original',
-    's_1',
     'profile_ebp_status',
     'profile_industry_id',
     'profile_weighted_total',
     'review_url',
-    'industry_name')
+    'industry_name']
+
+questions = [
+        's1',
+        's2',
+        's3',
+        's4',
+        's5',
+        's6',
+        's7',
+        's8',
+        's9',
+        's10',
+        's11',
+        's12',
+        's13']
+
+questions_mapping = [
+        'Vorgesetztenverhalten',
+        'Kollegenzusammenhalt',
+        'Interessante Aufgaben',
+        'Arbeitsatmosphäre',
+        'Kommunikation',
+        'Gleichberechtigung',
+        'Karriere/Weiterbildung',
+        'Gehalt/Sozialleistungen',
+        'Arbeitsbedingungen',
+        'Work-Life-Balance',
+        'Image',
+        'Umgang mit älteren Kollegen',
+        'Umwelt-/Sozialbewusstsein']
+
+mapping = dict(zip(questions, questions_mapping))
 
 
 def extract_document(
@@ -32,7 +61,8 @@ def extract_document(
     question: str | None = 's1',
     profile_uuid: str = '304de031-0ca6-45fe-b57e-99209d358551',
     min_length: int = 20,
-    columns: tuple = relevant_cols) -> list[Document]:
+    mapping: dict = mapping,
+    columns: list = relevant_cols) -> list[Document]:
 
     """
     ingest Dataframe from load_reviews.py and create a list of OS documents
@@ -43,14 +73,22 @@ def extract_document(
     @param columns: columns to add
     """
 
+    columns.append(f's_{question[1:]}')
+    columns.append(f'text_{question}')
+    columns.append(f'text_{question}_original')
+
+    print(columns)
+    print(data.head())
     # filter data
     data = data.loc[
     (data['profile_uuid'] == profile_uuid) &
     (data[f'text_{question}_len'] >= min_length) &
     (data[f'text_{question}_language'] == 'de') &
     (data[f'text_{question}_language_confidence'] >= .75),
-    relevant_cols]
+    columns]
 
+    if data.empty:
+        return None
 
     result = list()
 
@@ -62,6 +100,7 @@ def extract_document(
                 'ebp': values['profile_ebp_status'],
                 'industry': values['industry_name'],
                 'review_url': values['review_url'],
+                'category': mapping[question],
                 'stars_profile': values['profile_weighted_total']})
 
         result.append(d)
@@ -74,9 +113,21 @@ def extract_document(
 if __name__ == '__main__':
     from pprint import pprint
 
+    ds = OpenSearchDocumentStore(
+        host=credentials['host'],
+        username=credentials['user'],
+        password=credentials['password'],
+        index=config['review_index'])
+
+    ds.delete_index(config['review_index'])
+
+    ds = OpenSearchDocumentStore(
+        host=credentials['host'],
+        username=credentials['user'],
+        password=credentials['password'])
+
     with open('./data/processed_reviews.parquet', mode='rb') as file:
         df = pd.read_parquet(file)
-
 
     with open('./industry_mapping.csv', mode='r') as file:
         df_industry = pd.read_csv(file)
@@ -89,9 +140,15 @@ if __name__ == '__main__':
         left_on='profile_industry_id',
         right_on='industry_id')
 
-    test_extract = extract_document(df)
-    pprint(test_extract[0].to_dict())
-    
-    ds.write_documents(test_extract, index=config['review_index'])
+
+    for i in questions:
+
+        print(f'processing texts f{mapping[i]}')
+        extract = extract_document(df, question=i)
+
+        if extract:
+            pprint(extract[0].to_dict())
+            ds.write_documents(extract, index=config['review_index'])
+
     ds.describe_documents(index=config['review_index'])
 
